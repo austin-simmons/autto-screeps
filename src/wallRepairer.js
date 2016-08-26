847f02fe57d4b78c4f52d26c926f7fff1ba454a3
Austin Simmons <austin.simmons@verifi.com>
2016-08-26 12:47:48 -0700

    fixed wall repairer

--- src/wallRepairer.js
+++ src/wallRepairer.js
@@ -4,42 +4,48 @@ let wallRepairer = {
     /**
      * @param {Creep} creep
      */
-    run: (creep) => {
+    run: function(creep) {
         if (creep.memory.working == true && creep.carry.energy == 0) {
             creep.memory.working = false;
-        } else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+        }
+        else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
             creep.memory.working = true;
         }
 
-        let target;
+        if (creep.memory.working == true) {
+            let walls = creep.room.find(FIND_STRUCTURES, {
+                filter: (s) => s.structureType == STRUCTURE_WALL
+            });
 
-        if(creep.memory.working == true) {
-            // find most damaged walls first
-            for(let percentage = 0.0001; percentage <= 1; percentage += 0.0001) {
+            let target = undefined;
+
+            for (let percentage = 0.0001; percentage <= 1; percentage = percentage + 0.0001){
+
+
                 target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                     filter: (s) => s.structureType == STRUCTURE_WALL &&
-                                   s.hits / s.maxHits < percentage
+                                   s.hits / s.hitsMax < percentage
                 });
 
-                // break loop if one is found
-                if(target != undefined) {
+                if (target != undefined) {
                     break;
                 }
             }
 
-            if(target != undefined) {
-                if(creep.repair(target) == ERR_NOT_IN_RANGE) {
+            if (target != undefined) {
+                if (creep.repair(target) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target);
                 }
-            } else {
+            }
+            else {
                 builder.run(creep);
             }
-        } else {
+        }
+        else {
             let source = creep.pos.findClosestByPath(FIND_SOURCES, {
                 filter: (s) => s.energy > 0
             });
-
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source);
             }
         }

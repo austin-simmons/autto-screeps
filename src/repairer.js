f7197d42e14a3d6bb5ec51695e121b16a88aae70
Austin Simmons <autto91@gmail.com>
2016-08-25 20:36:40 -0700

    fixed repairer

--- src/repairer.js
+++ src/repairer.js
@@ -6,9 +6,9 @@ let repairer = {
      */
     run: (creep) => {
         if(creep.memory.working == true && creep.carry.energy == 0) {
-            creep.memory.working == false;
+            creep.memory.working = false;
         } else if(creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.working == true;
+            creep.memory.working = true;
         }
 
         if(creep.memory.working == true) {
